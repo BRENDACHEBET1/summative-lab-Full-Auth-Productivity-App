2a21e9dc0dd1716b8022ae2b7835f6710f4eb5fd
BRENDACHEBET1 <chebetb388@gmail.com>
2026-07-30 16:47:28 +0300

    Add user list and create endpoints

--- server/app.py
+++ server/app.py
@@ -8,3 +8,32 @@ from schemas import (
     exercise_schema,
     exercises_schema,
 )
+
+
+class Users(Resource):
+
+    # GET /users
+    def get(self):
+        users = User.query.all()
+
+        return users_schema.dump(users), 200
+
+
+    # POST /users
+    def post(self):
+
+        data = request.get_json()
+
+        user = User(
+            username=data["username"],
+            email=data["email"],
+            password=data["password"]
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return user_schema.dump(user), 201
+
+    #Enpoint
+api.add_resource(Users, "/users")
