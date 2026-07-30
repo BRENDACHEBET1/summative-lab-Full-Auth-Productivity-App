bdc406757ab53590ecee92a79037e7c97fb95999
BRENDACHEBET1 <chebetb388@gmail.com>
2026-07-30 13:00:01 +0300

    Add relationships

--- server/models.py
+++ server/models.py
@@ -14,7 +14,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
 
-
+    # One user can have many exercises
+    exercises = db.relationship(
+        "Exercise",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
 
     def __repr__(self):
         return f'User {self.username}, ID {self.id}'
@@ -27,3 +32,16 @@ class Exercise(db.Model):
     category = db.Column(db.String)
     duration = db.Column(db.Integer)  
     calories_burned = db.Column(db.Integer)
+
+    # Foreign key to users table
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
+
+     # Relationship back to User
+    user = db.relationship("User", back_populates="exercises")
+
+    def __repr__(self):
+        return f"Exercise {self.name}"
