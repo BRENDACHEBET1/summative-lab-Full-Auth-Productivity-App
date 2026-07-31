4a7b61c9294c23371623020ab243724ed80254a8
BRENDACHEBET1 <chebetb388@gmail.com>
2026-07-31 14:58:59 +0300

    Add login endpoint

--- server/app.py
+++ server/app.py
@@ -98,6 +98,29 @@ class UserByID(Resource):
 
 api.add_resource(UserByID, "/users/<int:id>")
 
+class Login(Resource):
+
+    def post(self):
+
+        data = request.get_json()
+
+        user = User.query.filter_by(
+            username=data["username"]
+        ).first()
+
+        if user and user.check_password(data["password"]):
+
+            session["user_id"] = user.id
+
+            return user_schema.dump(user), 200
+
+        return {
+            "error": "Invalid username or password"
+        }, 401
+
+
+api.add_resource(Login, "/login")
+
 
 class Exercises(Resource):
 
